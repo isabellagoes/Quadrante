e67d347e2d36adc7295f2f26a7926c842f46fb0c
isabellagoes <isabellagoes.igs@gmail.com>
2023-11-22 16:00:56 -0300

    Resolução de Exercício

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿double x, y;
+string local;
+
+Console.WriteLine("---------------- Quadrante Cartesiano ----------------\n");
+
+Console.WriteLine("O ponto pode se encontrar no plano cartesiano em:\n");
+
+Console.WriteLine("-> um dos quadrantes");
+Console.WriteLine("-> sobre um dos eixos");
+Console.WriteLine("-> na origem\n");
+
+Console.WriteLine("Informe as coordenadas vertical (y) e horizontal (x):\n");
+
+Console.Write("Coordenada x: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Coordenada y: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+if (x == 0 && y == 0) local = "na origem";
+else if (x == 0) local = "no eixo das ordenadas";
+else if (y == 0) local = "no eixo das abscissas";
+else if (y > 0)
+{
+    if (x > 0)
+        local = "no quadrante 1";
+    else
+        local = "no quadrante 2";
+}
+
+else
+{
+    if (x < 0)
+        local = "no quadrante 3";
+    else
+        local = "no quadrante 4";
+}
+
+Console.WriteLine($"\nO ponto ({x}, {y}) fica {local}.");
